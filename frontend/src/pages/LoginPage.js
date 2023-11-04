34d08f9e19eecdd4c943b43a2d68051791c93e23
Hariom Joshi <goodhariom@gmail.com>
2023-11-04 12:33:18 +0530

    Login page added

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,39 @@
+import "./LoginPage.css"
+import { useState } from "react";
+
+function LoginPage(){
+    let [passType, setPassType] = useState("password");
+    return(
+        <div className ="everyThing">
+            <div className="enterDetails">
+                <div className="centerPart">
+                    <h1 className="login">Hey, hello! 👋</h1>
+                    <p className="lightText">Enter the information you entered while registering</p>
+                    <legend className = "login">Email: </legend>
+                    <input  className="inp"/>
+                    <legend className = "login">Password: </legend>
+                    <input className = "inp" type = {passType}/>
+                    {/* <div className="showPassword">
+                        <p className = "lightText">Show password: </p>
+                        <input className="checkbox" type="checkbox" onClick={() =>{passType == "password"? setPassType("text"): setPassType("password")}} />    
+                    </div> */}
+                    <button className="loginBtn">Login</button>
+                </div>
+            
+            </div>
+
+
+            <div className="sideText">
+                
+                <div className="centerSquare">
+                    <h1 id="message"><div className="text">Draw<br/> and guess<br/> BETTER<br/></div>  and easier </h1>
+                    <p className="subHeading">Stream to any platform<br/> from anywhere</p>
+                </div>
+                
+            </div>
+        </div>
+
+    );
+}
+
+export default LoginPage;
